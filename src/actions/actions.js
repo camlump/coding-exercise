aa602ad7273eeae366fc006c8568bd71f5a14131
CameronLumpkinATL <camlump@yahoo.com>
2021-06-10 21:12:02 -0400

    started action for the app

---
+++ src/actions/actions.js
@@ -0,0 +1,10 @@
+
+//Action to update user info
+export const ADD_USER_INFO = 'ADD_USER_INFO';
+//action to select a time slot for
+export const CHOOSE_TIME_SLOT = 'CHOOSE_TIME_SLOT';
+
+//action for updating the state of the user info and time slot selected
+export const SUBMIT_USER_INFO = 'SUBMIT_USER_INFO';
+
+
